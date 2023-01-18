0aedd9a0b033cf17797765b8fa481fc4076b7653
T-erry <terrywachuka10@gmail.com>
2023-01-19 01:07:52 +0300

    completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorsList = directors.map((director)=>{
+    const {name, movies} = director
+    return <div key={name}>
+      <h1>{name}</h1>
+      <ul>
+        {movies.map((movie, index)=>{
+          return <li key={index}>{movie}</li>
+        })}
+      </ul>
+    </div>
+  })
+
+
+  return (
+    <>
+    <h1>Directors Page</h1>
+    {directorsList}
+    </>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  const movieList = movies.map((movie)=>{
+  return <div key={movie.title}>
+    <li>{movie.title}</li>
+    <p> Time: {movie.time}</p>
+    <ul>
+      {movie.genres.map((genre, index)=>{
+        return <li key={index}>{genre}</li>
+      })}
+    </ul>
+   </div>
+  })
+  return(
+    <>
+    <h1>Movies Page</h1>
+    {movieList}
+    </>
+  );
+    
 }
 
 export default Movies;
